187eec427bb25d7333f20b2a180be94ae5b15440
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2025-04-13 22:51:43 +0530

    ɴᴇᴡ-ᴜᴘᴅᴀᴛᴇ-ʙʏ-ᴀʟᴏɴᴇ-ᴄᴏᴅᴇʀ

--- AloneX/plugins/bot/settings.py
+++ AloneX/plugins/bot/settings.py
@@ -121,8 +121,8 @@ async def show_bot_info(c: app, q: CallbackQuery):
     delta_ping = time() - start
     await x.delete()
     txt = f"""
-    🏓 Pɪɴɢ: {delta_ping * 1000:.3f} ms   
-    🐍 Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ: 3.10.4
+    🏓 Pɪɴɢ: 3.1.0   
+    🐍 Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ: 4.10.4
     🔥 Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ: {pver}
     """
     await q.answer(txt, show_alert=True)
