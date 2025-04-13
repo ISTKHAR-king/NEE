f2a8279619734e2b1de38d575ade6f169d25e41d
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2025-04-13 23:03:23 +0530

    Update settings.py

--- AloneX/plugins/bot/settings.py
+++ AloneX/plugins/bot/settings.py
@@ -122,7 +122,7 @@ async def show_bot_info(c: app, q: CallbackQuery):
     await x.delete()
     txt = f"""
     🏓 Pɪɴɢ: {delta_ping * 1000:.3f} ms   
-    🐍 Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ: 3.10.4
+    🐍 Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ: 4.10.4
     🔥 Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ: 3.1.0
     """
     await q.answer(txt, show_alert=True)
